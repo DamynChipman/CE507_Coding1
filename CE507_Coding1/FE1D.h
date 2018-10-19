21849bc710e293fd11ba249c8aa8d39e291be34e
Damyn Chipman <damyn.chipman@gmail.com>
2018-10-18 19:56:02 -0600

    Added BC and fixed F vector for case B and C. Verified for N = 10, 100, 1000. Added documentation. Adding more outputs

--- CE507_Coding1/FE1D.h
+++ CE507_Coding1/FE1D.h
@@ -11,7 +11,7 @@
 
 //using namespace Eigen;
 
-Eigen::VectorXf FE1D(int* ID, int** IEN, int** LM, float (*k_ab_e)(int,int), float (*f_a_e)(int), int Ne) {
+Eigen::VectorXf FE1D(int* ID, int** IEN, int** LM, float (*k_ab_e)(int,int), float (*f_a_e)(int,int), int Ne) {
     
     // Initialize matrices
     Eigen::MatrixXf K(Ne,Ne);
@@ -24,20 +24,11 @@ Eigen::VectorXf FE1D(int* ID, int** IEN, int** LM, float (*k_ab_e)(int,int), flo
     }
     
     // Necessary variables
-    float k_ab = 0;
-    float f_a = 0;
     int P = 0;
     int Q = 0;
     
     // ----- Begin algorthim -----
-    // TODO: Add BC step
     for (int e = 0; e < Ne; e++) {
-        for (int a = 0; a < 2; a++) {
-            for (int b = 0; b < 2; b++) {
-                k_ab = k_ab_e(a,b); // Calc k_ab_e
-            }
-            f_a = f_a_e(a); // Calc f_a_e
-        }
         for (int a = 0; a < 2; a++) {
             P = LM[a][e]; // Integer mapping
             if (P != -1) {
@@ -47,10 +38,13 @@ Eigen::VectorXf FE1D(int* ID, int** IEN, int** LM, float (*k_ab_e)(int,int), flo
                         K(P,Q) = K(P,Q) + k_ab_e(a,b); // Update K matrix
                     }
                 }
-                F(P) = F(P) + f_a_e(a); // Update F vector
+                F(P) = F(P) + f_a_e(a,e); // Update F vector
             }
         }
     }
+    // Apply BC
+    P = LM[0][0];
+    F(P) = F(P) + f_a_e(0,0);
 
     // Solve system using Eigen
     Eigen::VectorXf d(Ne);
--- CE507_Coding1/main.cpp
+++ CE507_Coding1/main.cpp
@@ -19,6 +19,14 @@ using namespace Eigen;
 float DEL_X; // To be set later with specific discrete domain
 int N[4] = {10,100,1000,10000}; // Number of nodes
 
+// ----- Function Definitions -----
+/**
+ * @function kElement
+ * @brief k Element Matrix
+ * @param a : int : Function Index
+ * @param b : int : Node Index
+ * @returns mat(a,b) : float : Entry a index a,b
+ */
 float kElement(int a, int b) {
     Eigen::Matrix2f mat;
     
@@ -31,7 +39,14 @@ float kElement(int a, int b) {
     return mat(a,b);
 }
 
-float fElement1(int a) {
+/**
+ * @function fElement1
+ * @brief f Element Vector for f = c
+ * @param a : int : Function Index
+ * @param e : int : Element Index
+ * @returns vec(a) : float : Entry at index a
+ */
+float fElement1(int a, int e) {
     Eigen::Vector2f vec;
     float c = 1;
     float f1 = c;
@@ -44,12 +59,18 @@ float fElement1(int a) {
     return vec(a);
 }
 
-float fElement2(int a) {
+/**
+ * @function fElement2
+ * @brief f Element Vector for f = x
+ * @param a : int : Function Index
+ * @param e : int : Element Index
+ * @returns vec(a) : float : Entry at index a
+ */
+float fElement2(int a, int e) {
     Eigen::Vector2f vec;
     
-    float f1 = 0;
-    float f2 = 1;
-    // TODO: Fix these values
+    float f1 = (e)*DEL_X;
+    float f2 = (e+1)*DEL_X;
     float value = DEL_X/6;
     vec(0) = value*(2*f1 + f2);
     vec(1) = value*(f1 + 2*f2);
@@ -57,17 +78,38 @@ float fElement2(int a) {
     return vec(a);
 }
 
-float fElement3(int a) {
+/**
+ * @function fElement3
+ * @brief f Element Vector for f = x^2
+ * @param a : int : Function Index
+ * @param e : int : Element Index
+ * @returns vec(a) : float : Entry at index a
+ */
+float fElement3(int a, int e) {
     Eigen::Vector2f vec;
-    // TODO: Fix these values
-    float f1 = 0;
-    float f2 = 1;
+    float f1 = (e*DEL_X)*(e*DEL_X);
+    float f2 = ((e+1)*DEL_X)*((e+1)*DEL_X);
     
     float value = DEL_X/6;
     vec(0) = value*(2*f1 + f2);
     vec(1) = value*(f1 + 2*f2);
     
     return vec(a);
+}
+
+/**
+ * @function uActual
+ * @brief Actual solution to problem
+ * @param x : float : Point to evalute
+ * @param p : int : Parameter for f
+ * @returns value : float : Value of solution
+ */
+float uActual(float x, int p) {
+    float c = 1;
+    if (p == 1) { return 0.5*c*(1 - x*x); }
+    else if (p == 2) { return (1.0/6.0)*(1 - x*x); }
+    else if (p == 3) { return (1.0/12.0)*(1 - x*x*x*x); }
+    else { return 0;}
 }
 
 int main(int argc, const char * argv[]) {
@@ -77,13 +119,20 @@ int main(int argc, const char * argv[]) {
     string u1Names[4] = {"u1N1.csv","u1N2.csv","u1N3.csv","u1N4.csv"};
     string u2Names[4] = {"u2N1.csv","u2N2.csv","u2N3.csv","u2N4.csv"};
     string u3Names[4] = {"u3N1.csv","u3N2.csv","u3N3.csv","u3N4.csv"};
+    string uAct1Names[4] = {"uAct1N1.csv","uAct1N2.csv","uAct1N3.csv","uAct1N4.csv"};
+    string uAct2Names[4] = {"uAct2N1.csv","uAct2N2.csv","uAct2N3.csv","uAct2N4.csv"};
+    string uAct3Names[4] = {"uAct3N1.csv","uAct3N2.csv","uAct3N3.csv","uAct3N4.csv"};
+    string IDNames[4] = {"IDN1.csv","IDN2.csv","IDN3.csv","IDN4.csv"};
+    string IENNames[4] = {"IENN1.csv","IENN2.csv","IENN3.csv","IENN4.csv"};
+    string LMNames[4] = {"LMN1.csv","LMN2.csv","LMN3.csv","LMN4.csv"};
     
-    clock_t t;
-    t = clock();
     // --- BEGIN LOOP ---
     cout << " --- BEGINNING PROGRAM LOOP --- " << endl;
-    for (int n = 0; n < 3; n++) {
+    clock_t t;   // Timing
+    t = clock(); // Timing
+    for (int n = 0; n < 4; n++) {
         cout << " n = " << n << " ---------- N = " << N[n] << " ------------ " << endl;
+        
         // Create function and plotting domain
         cout << "   generating domains... " << endl;
         float xL = 0;
@@ -123,12 +172,10 @@ int main(int argc, const char * argv[]) {
             }
         }
 
-        // Perform FE
+        // Perform FE1D
         cout << "   calculating coefs1... " << endl;
         Eigen::VectorXf coefs1 = FE1D(ID, IEN, LM, kElement, fElement1, N[n]); // For f = c
-        cout << "   calculating coefs2... " << endl;
         Eigen::VectorXf coefs2 = FE1D(ID, IEN, LM, kElement, fElement2, N[n]); // For f = x
-        cout << "   calculating coefs3... " << endl;
         Eigen::VectorXf coefs3 = FE1D(ID, IEN, LM, kElement, fElement3, N[n]); // For f = x^2
         
         // Generate solution
@@ -136,10 +183,16 @@ int main(int argc, const char * argv[]) {
         float* u_h1 = (float*)malloc(NPlot * sizeof(float));
         float* u_h2 = (float*)malloc(NPlot * sizeof(float));
         float* u_h3 = (float*)malloc(NPlot * sizeof(float));
+        float* u_act1 = (float*)malloc(NPlot * sizeof(float));
+        float* u_act2 = (float*)malloc(NPlot * sizeof(float));
+        float* u_act3 = (float*)malloc(NPlot * sizeof(float));
         for (int i = 0; i < NPlot; i++) {
             u_h1[i] = 0;
             u_h2[i] = 0;
             u_h3[i] = 0;
+            u_act1[i] = uActual(plotDomain.x_h_[i], 1);
+            u_act2[i] = uActual(plotDomain.x_h_[i], 2);
+            u_act3[i] = uActual(plotDomain.x_h_[i], 3);
             for (int j = 0; j < N[n]; j++) {
                 u_h1[i] = u_h1[i] + coefs1(j)*Nfunction.eval(j, plotDomain.x_h_[i]);
                 u_h2[i] = u_h2[i] + coefs2(j)*Nfunction.eval(j, plotDomain.x_h_[i]);
@@ -153,22 +206,64 @@ int main(int argc, const char * argv[]) {
         ofstream u1(u1Names[n]);
         ofstream u2(u2Names[n]);
         ofstream u3(u3Names[n]);
+        ofstream uAct1(uAct1Names[n]);
+        ofstream uAct2(uAct2Names[n]);
+        ofstream uAct3(uAct3Names[n]);
+        ofstream IDOutput(IDNames[n]);
+        ofstream IENOutput(IENNames[n]);
+        ofstream LMOutput(LMNames[n]);
         char delim = ',';
         
         for (int i = 0; i < NPlot; i++) {
             x << plotDomain.x_h_[i] << delim;
             u1 << u_h1[i] << delim;
             u2 << u_h2[i] << delim;
-            u2 << u_h3[i] << delim;
+            u3 << u_h3[i] << delim;
+            uAct1 << u_act1[i] << delim;
+            uAct2 << u_act2[i] << delim;
+            uAct3 << u_act3[i] << delim;
+        }
+        
+        for (int i = 0; i < N[n]; i++) {
+            IDOutput << ID[i] << delim;
+            for (int j = 0; j < 2; j++) {
+                IENOutput << IEN[j][i] << delim;
+                LMOutput << LM[j][i] << delim;
+            }
+            IENOutput << endl;
+            LMOutput << endl;
         }
         
         x.close();
         u1.close();
         u2.close();
         u3.close();
+        uAct1.close();
+        uAct2.close();
+        uAct3.close();
+        IDOutput.close();
+        IENOutput.close();
+        LMOutput.close();
+        
+        // Memory
+        free(ID);
+        for (int i = 0; i < 2; i++) {
+            delete [] IEN[i];
+            delete [] LM[i];
+        }
+        delete [] IEN;
+        delete [] LM;
+        free(u_h1);
+        free(u_h2);
+        free(u_h3);
+        free(u_act1);
+        free(u_act2);
+        free(u_act3);
+        
+        // Timing
+        t = clock() - t;
+        cout << "Previous loop took " << ((float)t/CLOCKS_PER_SEC) << " seconds" << endl;
     }
-    t = clock() - t;
-    cout << "Previous loop took " << ((float)t/CLOCKS_PER_SEC) << " seconds" << endl;
     
     return 0;
 }
